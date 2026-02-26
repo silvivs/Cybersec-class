514a8c8ca6296d7e5e2e95afb647933603c343f8
Philipe Silva <philjpes@gmail.com>
2026-02-26 19:47:07 -0400

    fixing php:7.4-apache in docker-compose.yml

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h1>YARIPO CORP - Securing Your Future</h1>";
-echo "<p>Bem-vindo ao portal institucional. Área restrita para funcionários.</p>";
+echo "<p>Welcome to the institutional portal. Restricted area for employees..</p>";
 // No futuro, vamos colocar um formulário de login vulnerável aqui!
 ?>
